4d377b5975612e68f6803099c289b7fdde50000e
Riday1 <rrriday100@gmail.com>
2024-11-17 01:34:42 +0600

    problem-8

--- assignment1/src/problem8.ts
+++ assignment1/src/problem8.ts
@@ -2,8 +2,23 @@
     //
 
     function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
+        let count: number = 0;
+        const resultArray = keys.filter(key => {
+            if (key in obj) {
+                return true
+            }
+            else {
+                count++
+                return false
+            }
+        })
 
-        return keys.every(key => key in obj);
+        if (count > 0) {
+            return true
+        }
+        else {
+            return false
+        }
 
     }
     const person = { name: "Alice", age: 25, email: "x@gmail.com" }
