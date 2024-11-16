80ffe2f719aa51f129681da812a2544ccfcd5373
Riday1 <rrriday100@gmail.com>
2024-11-16 20:52:37 +0600

    to solve problem 3 i have used split to slplit the whole sentence and filter method to find the word

--- assignment1/src/problem3.ts
+++ assignment1/src/problem3.ts
@@ -1,9 +1,15 @@
 {
     //
 
-    function countWordOccurrences(sentence: string, word: string){
-        
+    function countWordOccurrences(sentence: string, word: string) {
+        const covertSentenceIntoWord = sentence.split(" ")
+        // console.log(covertSentenceIntoWord)
+        const wordArray = covertSentenceIntoWord.filter(singleWord => singleWord === word)
+        const count = wordArray.length
+        return count
     }
+    // console.log(countWordOccurrences("i love to play cricket  because cricket is a wellknown game in the world ", "cricket"));
+
 
 
 }
